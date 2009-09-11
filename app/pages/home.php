41d276cfc28da3b5e3fcbaea16e68f4ee5131e91
Judd Vinet <jvinet@zeroflux.org>
2009-09-11 11:01:35 -0700

    reverted test code

--- app/pages/home.php
+++ app/pages/home.php
@@ -6,13 +6,6 @@ class pHome extends Page_Static
 	{
 		$this->set_dir('home');
 	}
-
-	function GET()  {
-		$this->plugins->mailer->send('jvinet@zeroflux.org', 'SendGrid Test',
-			'This is a test', 'jvinet@zeroflux.org');
-		echo 'Sent';
-		$this->render('home/index.php');
-	}
 }
 
 
