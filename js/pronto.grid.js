c766b1f09a0d1e5800e9b35a4966fdfef4676dc9
Judd Vinet <jvinet@zeroflux.org>
2010-05-29 00:32:25 -0300

    fixed up grid stuff (somewhat) so it's actually usable directly

--- js/pronto.grid.js
+++ js/pronto.grid.js
@@ -29,7 +29,8 @@ function grid_dispatch(evt) {
 	// call the URL in the event target and act on the JSON results
 	$.ajaxSetup({dataType: 'json'});
 	$.get(url, {_ajax: 1}, function(data){
-		if(data.html) grid_loadhtml(targ, data.html);
+		var $tr = $(targ).parent().parent().parent();
+		if(data.html) grid_loadhtml($tr, data.html);
 		if(data.js) eval(data.js);
 	});
 	return false;
@@ -39,15 +40,18 @@ function grid_dispatch(evt) {
  * Load some HTML (usually a form) into a content space below the
  * selected row.  If a form is present in the HTML, it will be
  * AJAX-enabled.
+ *
+ * The 'targ' argument should be the <TR> element that will contain
+ * the new content.
  */
 function grid_loadhtml(targ, html) {
-	var targ_id = $(targ).parent().parent().parent().attr('id');
-	var td_id = "#"+targ_id+"_form_td";
+	var tr_id = $(targ).attr('id');
+	var td_id = "#"+tr_id+"_form_td";
 	var close_str = "<br /><br />"+
 		"<a href=\"#\" id=\""+td_id.substring(1)+"_close\" style=\"text-decoration:none;color:#00f\">"+
 		"&#8593; Close &#8593;</a>";
 
-	$(targ).parent().parent().parent().addClass('selected');
+	$(targ).addClass('selected');
 	$(td_id).empty();
 	$(td_id).append(html + close_str);
 	$(td_id).show();
@@ -60,13 +64,15 @@ function grid_loadhtml(targ, html) {
 
 /**
  * Remove the content/HTML region previously created by grid_loadhtml().
+ *
+ * The 'targ' argument should be the <TR> element that will be removed.
  */
 function grid_closehtml(targ) {
 	if(typeof targ == 'string') {
 		var td_id = targ;
 	} else {
-		var targ_id = $(targ).parent().parent().parent().attr('id');
-		var td_id = "#"+targ_id+"_form_td";
+		var tr_id = $(targ).attr('id');
+		var td_id = "#"+tr_id+"_form_td";
 	}
 	$(td_id).hide();
 	$(td_id).empty();
