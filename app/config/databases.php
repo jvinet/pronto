f0a1f8f06c09c10e81ca4c35e6e7ef7c6877117f
Judd Vinet <judd@betsmartmedia.com>
2012-08-13 10:47:52 -0700

    as the mysql extension is now discouraged, changed the default database config to use PDO

--- app/config/databases.php
+++ app/config/databases.php
@@ -2,22 +2,14 @@
 
 /**
  * DATABASE CONNECTIONS
- *
- * NOTE: If you're using MySQL and you want to connect to multiple
- * databases using the same host/user/pass connection parameters, then
- * you may run into some side effects, as PHP will implicitly use the same
- * connection internally.  This can result in queries being executed on the
- * wrong database.  To work around this, use slightly different connection
- * values (eg, "127.0.0.1" instead of "localhost") for each connection.
  */
 
 $DATABASES = array(
 	'main' => array(
-		'type' => 'mysql',
-		'host' => 'localhost',
+		'type' => 'pdo',
+		'dsn'  => 'mysql:host=localhost;dbname=pronto',
 		'user' => 'root',
 		'pass' => '',
-		'name' => 'pronto',
 		'opts' => array('persistent' => true)
 	),
 
